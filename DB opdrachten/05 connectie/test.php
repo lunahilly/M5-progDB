8e762a47053b52270bada79da6cf15960c01690a
lunahilly <35861@ma-web.nl>
2023-10-28 19:36:02 +0200

    opdracht 5

---
+++ DB opdrachten/05 connectie/test.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once("dbdata.php");
+
+$conn = new mysqli($databaseServer, $user, $pass, $schema);
+if ($conn->connect_error) {
+    die("error try again\r\n");
+}
+
+echo"connected to database\r\n";
+
+$conn->close();
+
+?>
